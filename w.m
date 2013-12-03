9b6710795a05350b27a5f4bd45575955e5d25d88
Jim Bracewell <jimbracewell@Jims-MacBook-Air.local>
2013-12-03 10:57:40 +0000

    cleaned up w code

--- w.m
+++ w.m
@@ -2,13 +2,13 @@
 %wMatrix - t x 2 matrix which holds the index of the pattern in the first
 %column, and the value of alpha in the second column
 %kernelMatrix - holds the values of all possible kernel calls
-% t -  current time
-%  x - the index of the pattern we are calculating w for
+% t -  current time (also index of pattern x)
 
-function rtn = w(wMatrix, kernelMatrix, t, x)
+
+function rtn = w(wMatrix, kernelMatrix, t)
 value = 0;
-for i = 1:t
-    value = value + (wMatrix(i,2) * KernelMatrix(wMatrix(i,1), x));
+for i = 1:(t-1)
+    value = value + (wMatrix(i,2) * KernelMatrix(wMatrix(i,1), t));
     
 end
 
